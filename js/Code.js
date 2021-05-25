9a8afd05bb5b677e439d18ef96d99ced635fa54c
HarshilMehta14 <75520512+HarshilMehta14@users.noreply.github.com>
2021-05-25 16:40:39 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70);
+// add code for displaying rest of the names and the hints.
+
+    text("C F I U N T O N", 700,150);
+    text("Hint: Helps you out a lot, makes the code more readable !!", 700,170);
+
+    text("B T A D S E A A", 100,350);
+    text("Hint: A lot of apps and games have this, Helps the app !!", 100,370);
+}
